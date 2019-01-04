8bb8218b9f3101a99f51f54612b68389d2b0f457
joshuarossi <joshua.rossi@icloud.com>
2019-01-04 18:14:41 -0500

    update index and README

--- README.md
+++ README.md
@@ -34,8 +34,6 @@ p {
 import base from './base.module.css';
 import custom from './custom.module.css';
 
-console.log(base);
-console.log(custom);
 const styles = { ...base, ...custom };
 console.log(styles);
 
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,9 +1,7 @@
-import base from './base.module.css'
-import custom from './custom.module.css'
+import base from './base.module.css';
+import custom from './custom.module.css';
 
-console.log(base)
-console.log(custom)
-const styles = {...base, ...custom}
-console.log(styles)
+const styles = { ...base, ...custom };
+console.log(styles);
 
-export default styles
+export default styles;
