01c8defb8fff1d711ca173c3ed9fce642823f16a
joshuarossi <joshua.rossi@icloud.com>
2019-01-04 18:42:29 -0500

    move app styles to module

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import styles from './styles';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={styles.App}>
+        <header className={styles.AppHeader}>
+          <img src={logo} className={styles.AppLogo} alt="logo" />
           <p className={styles.test}>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <p>Hello, World!</p>
           <a
-            className="App-link"
+            className={styles.AppLink}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer">
