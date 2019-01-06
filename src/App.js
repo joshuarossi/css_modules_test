e677b2f96ed884b8ba0b3782b1c81a84b6bf2c85
joshuarossi <joshua.rossi@icloud.com>
2019-01-06 14:20:14 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import styles from './styles';
 
+const { app, appHeader, appLink, appLogo, test } = styles;
 class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <header className={styles.AppHeader}>
-          <img src={logo} className={styles.AppLogo} alt="logo" />
-          <p className={styles.test}>
+      <div className={app}>
+        <header className={appHeader}>
+          <img src={logo} className={appLogo} alt="logo" />
+          <p className={test}>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <p>Hello, World!</p>
           <a
-            className={styles.AppLink}
+            className={appLink}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer">
